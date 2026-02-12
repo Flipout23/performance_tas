72366d95df88c1b8ef5767a6193b4d30254fd75b
Flipout23 <whenlimit11@gmail.com>
2026-02-12 14:51:22 +0000

    closer

--- performance.py
+++ performance.py
@@ -4,29 +4,29 @@ def medical_help(illness):
         "covid-19": ["To treat yourself, do this: ", "Over-the-counter medications like acetaminophen and ibuprofen can help relieve fever and body aches.", "Staying hydrated is important.", "It is also important to avoid high-sugar drinks and processed foods."  ], 
         "flu": ["To treat yourself, do this: ", "Nasal Spray and humidifiers can relieve congestion and discomfort.", "Use cough suppressants for dry coughs or expectorants to loosen mucus in the throat.", "Natural remedies include chicken soup, ginger tea, and zinc supplements which may help ease symptoms and support immune function" ],
         "allergies": ["To treat yourself, do this: ", "Avoid exposure to allergens like pollen, dust mites, and pet dander.", "Use antihistamines or nasal sprays to reduce symptoms.", "Consult a doctor for severe or persistent allergies."],
-    } #This is our list with the medical advice we will be referencing and referring to in the future
+    } #This list function uses a dictionary to store the medical advice for each illness, and it will allow the user to easily access the advice for the illness they are asking about.
     
     if illness in medical_advice:
         for line in medical_advice[illness]:
-            input(line) #This will show the user the medical advice for the illness they are asking about.
+            input(line) #This if statement checks if the illness that the user is asking about is in the medical advice dictionary, and if it is, it will print out the advice for that illness. 
     else:
-        print("Sorry, we don't have treatment for that illness.") # This shows the user that we do not have advice for the illness they are asking about.
-print("Welcome! We are here to help you with medical advice!") #This is the welcome message for the user when they start the program.
-choice = input("Do you need medical assistance? (yes/no) ")#This is the first question that the user will be asked, and it will determine if they will be able to get medical advice or not.
+        print("Sorry, we don't have treatment for that illness.") # This else statement will print out a message if the user asks about an illness that is not in the medical advice dictionary.
+print("Welcome! We are here to help you with medical advice!") #This prints the welcome message for the user when they start the program.
+choice = input("Do you need medical assistance? (yes/no) ")#the 'choice=input' allows for the user to input whether they need medical assistance or not, and it will determine the flow of the program based on their answer.
 if choice == "no":
     print("Okay, stay healthy and fit!") # This if statement wil end the program if the user does not need medical help.
 while choice == "yes":
     print("We are here to help you!")
     question = input("Do you need help with a covid-19, flu, or allergies? ")
     medical_help(question)
-    choice = input("Do you need more medical advice? (yes/finished) ") # This while loop will allow the user to ask for more medical advice if they need it, and it will end the program if they are finished.
+    choice = input("Do you need more medical advice? (yes/finished) ") # This while loop uses the 'choice=input' to allow the user to ask for more medical advice if they need it, and it will continue to loop until the user is finished asking for medical advice.
 if choice == "finished":
-    rate = int(input("Please rate our service 1-10! "))
+    rate = int(input("Please rate our service 1-10! ")) # "int(input" allows the user to input a rating for the service, and it will convert that input into an integer so that it can be used to calculate the final score.)"
     final_score = int(rate * 10)
     print(str(final_score) + " percent satisfaction rate")
-    friend = input("Would you recommend our service to a friend? (yes/maybe/no) ") #This is the final question of the program, and it will ask the user if they would recommend our service to a friend.
+    friend = input("Would you recommend our service to a friend? (yes/maybe/no) ") #the friend=input allows the user to input whether they would recommend the service to a friend or not, and it will determine the final message that the program will print out based on their answer.
 
     if friend == "yes" or friend == "maybe":
         print("Thanks, we appreciate it. ")
     else:
-        print("Sorry we could not meet your expectations. ") #This is the end of the program, and it will thank the user for their feedback or apologize if they were not satisfied with the service.
+        print("Sorry we could not meet your expectations. ") #this if/else statement will print out a message based on whether the user would recommend the service to a friend or not, and it will thank them if they would recommend it, or apologize if they would not recommend it.
